6c2c4b3e7d795b9a09fb3dd53e95c1995b90428e
Matteo De Benedetti <matteo.de.benedetti@esa.int>
2019-11-13 17:52:45 +0100

    added script to generate test sequences
    this ruby script allows to move the rover using the motion_generator task and record imu, vicon and camera to be used for a VO test with virtual_spartan.rb

---
+++ scripts/vo_test_gen.rb
@@ -0,0 +1,139 @@
+#!/usr/bin/env ruby
+
+require 'orocos'
+require 'rock/bundle'
+require 'readline'
+include Orocos
+
+options = {:v => true}
+
+Bundles.initialize
+
+Bundles.transformer.load_conf(Bundles.find_file('config', 'transforms_scripts_exoter.rb'))
+
+Orocos::Process.run 'control', 'loccam', 'imu', 'navigation', 'vicon::Task' => 'vicon', 'motion_generator::Task' => 'motion_generator' do
+
+    motion_generator = Orocos.name_service.get 'motion_generator'
+    Orocos.conf.apply(motion_generator, ['default'], :override => true)
+    motion_generator.configure
+
+    locomotion_control = Orocos.name_service.get 'locomotion_control'
+    Orocos.conf.apply(locomotion_control, ['exoter'], :override => true)
+    locomotion_control.configure
+
+    wheel_walking_control = Orocos.name_service.get 'wheel_walking_control'
+    Orocos.conf.apply(wheel_walking_control, ['exoter'], :override => true)
+    wheel_walking_control.configure
+
+    locomotion_switcher = Orocos.name_service.get 'locomotion_switcher'
+    Orocos.conf.apply(locomotion_switcher, ['default'], :override => true)
+    locomotion_switcher.configure
+
+    command_joint_dispatcher = Orocos.name_service.get 'command_joint_dispatcher'
+    Orocos.conf.apply(command_joint_dispatcher, ['exoter_commanding'], :override => true)
+    command_joint_dispatcher.configure
+
+    platform_driver = Orocos.name_service.get 'platform_driver_exoter'
+    Orocos.conf.apply(platform_driver, ['arm'], :override => true)
+    platform_driver.configure
+
+    read_joint_dispatcher = Orocos.name_service.get 'read_joint_dispatcher'
+    Orocos.conf.apply(read_joint_dispatcher, ['exoter_reading'], :override => true)
+    read_joint_dispatcher.configure
+
+    ptu_control = Orocos.name_service.get 'ptu_control'
+    Orocos.conf.apply(ptu_control, ['default'], :override => true)
+    ptu_control.configure
+
+    imu_stim300 = TaskContext.get 'imu_stim300'
+    Orocos.conf.apply(imu_stim300, ['default', 'exoter', 'ESTEC', 'stim300_5g'], :override => true)
+    imu_stim300.configure
+
+#    visual_odometry = TaskContext.get 'viso2'
+#    Orocos.conf.apply(visual_odometry, ['default','bumblebee'], :override => true)
+#    Bundles.transformer.setup(visual_odometry)
+#    visual_odometry.configure
+#
+#    viso2_with_imu = TaskContext.get 'viso2_with_imu'
+#    Orocos.conf.apply(viso2_with_imu, ['default'], :override => true)
+#    viso2_with_imu.configure
+#
+#    viso2_evaluation = TaskContext.get 'viso2_evaluation'
+#    Orocos.conf.apply(viso2_evaluation, ['default'], :override => true)
+#    viso2_evaluation.configure
+
+    vicon = Orocos.name_service.get 'vicon'
+    Orocos.conf.apply(vicon, ['default','exoter'], :override => true)
+    vicon.configure
+
+    camera_firewire_loccam = TaskContext.get 'camera_firewire_loccam'
+    #Orocos.conf.apply(camera_firewire_loccam, ['exoter_bb2_b','auto_exposure'], :override => true)
+    Orocos.conf.apply(camera_firewire_loccam, ['exoter_bb2_b'], :override => true)
+    camera_firewire_loccam.configure
+
+    camera_loccam = TaskContext.get 'camera_loccam'
+    Orocos.conf.apply(camera_loccam, ['hdpr_bb2'], :override => true)
+    camera_loccam.configure
+
+    Orocos.log_all
+
+
+    #motion_generator.ptu_command.connect_to            ptu_control.ptu_joints_commands
+    ptu_control.ptu_commands_out.connect_to             command_joint_dispatcher.ptu_commands
+
+    motion_generator.motion_command.connect_to          locomotion_switcher.motion_command
+    motion_generator.locomotion_mode.connect_to         locomotion_switcher.locomotion_mode_override
+
+   locomotion_switcher.kill_switch.connect_to           wheel_walking_control.kill_switch
+   locomotion_switcher.reset_dep_joints.connect_to      wheel_walking_control.reset_dep_joints
+   locomotion_switcher.lc_motion_command.connect_to     locomotion_control.motion_command
+   read_joint_dispatcher.joints_samples.connect_to      locomotion_switcher.joints_readings
+   read_joint_dispatcher.motors_samples.connect_to      locomotion_switcher.motors_readings
+   read_joint_dispatcher.joints_samples.connect_to      wheel_walking_control.joint_readings
+
+   locomotion_control.joints_commands.connect_to        locomotion_switcher.lc_joints_commands
+   wheel_walking_control.joint_commands.connect_to      locomotion_switcher.ww_joints_commands
+
+   locomotion_switcher.joints_commands.connect_to       command_joint_dispatcher.joints_commands
+
+   command_joint_dispatcher.motors_commands.connect_to  platform_driver.joints_commands
+   platform_driver.joints_readings.connect_to           read_joint_dispatcher.joints_readings
+   read_joint_dispatcher.motors_samples.connect_to      locomotion_control.joints_readings
+   read_joint_dispatcher.ptu_samples.connect_to         ptu_control.ptu_samples
+
+   camera_firewire_loccam.frame.connect_to              camera_loccam.frame_in
+
+#   camera_loccam.left_frame.connect_to                 visual_odometry.left_frame
+#   camera_loccam.right_frame.connect_to                visual_odometry.right_frame
+#
+#   motion_generator.motion_command.connect_to          visual_odometry.motion_command
+#   visual_odometry.delta_pose_samples_out.connect_to   viso2_with_imu.delta_pose_samples_in
+#   imu_stim300.orientation_samples_out.connect_to      viso2_with_imu.pose_samples_imu
+#
+#   vicon.pose_samples.connect_to                       viso2_evaluation.groundtruth_pose
+#   viso2_with_imu.pose_samples_out.connect_to          viso2_evaluation.odometry_pose
+#   visual_odometry.pose_samples_out.connect_to         viso2_evaluation.odometry_pose
+
+    platform_driver.start
+    read_joint_dispatcher.start
+    command_joint_dispatcher.start
+    locomotion_control.start
+    wheel_walking_control.start
+    locomotion_switcher.start
+    ptu_control.start
+    imu_stim300.start
+
+    camera_firewire_loccam.start
+    camera_loccam.start
+
+#   visual_odometry.start
+#   viso2_with_imu.start
+#   viso2_evaluation.start
+    vicon.start
+
+    motion_generator.start
+
+    Readline::readline("Press Enter to exit\n") do
+    end
+
+end
