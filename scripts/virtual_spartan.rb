9cb9c309a4cf09050dc79acf63e6c9d6429339cd
Matteo De Benedetti <matteo.de.benedetti@esa.int>
2019-11-22 18:57:16 +0100

    added the imu

--- scripts/virtual_spartan.rb
+++ scripts/virtual_spartan.rb
@@ -9,18 +9,17 @@ include Orocos
 options = {:v => true}
 
 Bundles.initialize
-tfse_file = Bundles.find_file('config', 'transforms_scripts_exoter.rb')
+tfse_file = '/home/marta/rock/bundles/rover/config/transforms_scripts_exoter.rb'
 Bundles.transformer.load_conf(tfse_file)
 
 # open log file 
-log = Orocos::Log::Replay.open('~/rock/bundles/exoter/logs/20191101-1513/loccam.0.log', 
-                               '~/rock/bundles/exoter/logs/20191101-1513/motion_generator.0.log', 
-                               '~/rock/bundles/exoter/logs/20191101-1513/imu.0.log',
-                               '~/rock/bundles/exoter/logs/20191101-1513/vicon.0.log')
+log = Orocos::Log::Replay.open('~/rock/bundles/exoter/logs/20191121-1715/loccam.0.log', 
+                               '~/rock/bundles/exoter/logs/20191121-1715/imu.0.log',
+                               '~/rock/bundles/exoter/logs/20191121-1715/vicon.0.log')
 
 log.use_sample_time = true
 
-Orocos::Process.run 'imu', 'viso2_with_imu::Task' => 'viso2_with_imu', 'stereo_vo::SpartanVO' => 'spartan', 'viso2_evaluation::Task' => ['viso2_evaluation','viso2_evaluation_imu'] do
+Orocos::Process.run 'viso2_with_imu::Task' => 'viso2_with_imu', 'spartan::Task' => 'spartan', 'viso2_evaluation::Task' => ['viso2_evaluation','viso2_evaluation_imu'] do
 
     # CONFIGURE TASKS
     viso2_evaluation = Orocos.name_service.get 'viso2_evaluation'
@@ -36,11 +35,9 @@ Orocos::Process.run 'imu', 'viso2_with_imu::Task' => 'viso2_with_imu', 'stereo_v
     viso2_with_imu.configure
 
     spartan = Orocos.name_service.get 'spartan'
+    spartan.apply_conf_file("/home/marta/rock/perception/orogen/spartan/config/spartan::Task.yml", ["default"])
+    Orocos.transformer.setup(spartan);
     spartan.configure
-
-    imu_stim300 = TaskContext.get 'imu_stim300'
-    Orocos.conf.apply(imu_stim300, ['default', 'exoter', 'ESTEC', 'stim300_5g'], :override => true    )
-    imu_stim300.configure
 
     Orocos.log_all_ports
     
@@ -49,13 +46,11 @@ Orocos::Process.run 'imu', 'viso2_with_imu::Task' => 'viso2_with_imu', 'stereo_v
     log.camera_loccam.left_frame.connect_to             spartan.img_in_left 
     log.camera_loccam.right_frame.connect_to            spartan.img_in_right
 
-    log.motion_generator.motion_command.connect_to      spartan.motion_command
-    
     log.vicon.pose_samples.connect_to                   viso2_evaluation.groundtruth_pose
     spartan.vo_out.connect_to                           viso2_evaluation.odometry_pose
     
     spartan.delta_vo_out.connect_to                     viso2_with_imu.delta_pose_samples_in
-    imu_stim300.orientation_samples_out.connect_to      viso2_with_imu.pose_samples_imu
+    log.imu_stim300.orientation_samples_out.connect_to  viso2_with_imu.pose_samples_imu
     
     viso2_with_imu.pose_samples_out.connect_to          viso2_evaluation_imu.odometry_pose
     log.vicon.pose_samples.connect_to                   viso2_evaluation_imu.groundtruth_pose
@@ -63,10 +58,9 @@ Orocos::Process.run 'imu', 'viso2_with_imu::Task' => 'viso2_with_imu', 'stereo_v
 
     # START TASKS
     spartan.start
+    viso2_with_imu.start
     viso2_evaluation.start
     viso2_evaluation_imu.start
-    imu_stim300.start
-    viso2_with_imu.start
 
 
     # START LOG REPLAY
