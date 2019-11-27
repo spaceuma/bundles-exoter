2b8712182669873ee2d54bbe950fff64bac6cf57
Matteo De Benedetti <matteo.de.benedetti@esa.int>
2019-11-27 11:32:08 +0100

    updated the motion generator test script to include the new config file

--- scripts/motion_gen_test.rb
+++ scripts/motion_gen_test.rb
@@ -9,9 +9,14 @@ options = {:v => true}
 
 Bundles.initialize
 
+#ENV['LANG'] = 'C'
+#ENV['LC_NUMERIC'] = 'C'
+
 Orocos::Process.run 'motion_generator::Task' => 'motion_generator' do
 
     motion_generator = Orocos.name_service.get 'motion_generator'
+    #motion_generator.apply_conf_file("config/motion_generator::MotionCommands.yml", ["default"])
+    Orocos.conf.apply(motion_generator, ['default'], :override => true)
     motion_generator.configure
     motion_generator.start
 
