fdf579119d201912e40583ad4cf962470c5d3742
Matteo De Benedetti <matteo.de.benedetti@esa.int>
2019-12-10 11:03:46 +0100

    updated the virtual spartan script to run with the new with_imu and evaluation tasks with stream aligner

--- scripts/virtual_spartan.rb
+++ scripts/virtual_spartan.rb
@@ -13,19 +13,15 @@ tfse_file = '/home/marta/rock/bundles/rover/config/transforms_scripts_exoter.rb'
 Bundles.transformer.load_conf(tfse_file)
 
 # open log file 
-log = Orocos::Log::Replay.open('~/rock/bundles/exoter/logs/20191129-1036/loccam.0.log', 
-                               '~/rock/bundles/exoter/logs/20191129-1036/imu.0.log',
-                               '~/rock/bundles/exoter/logs/20191129-1036/vicon.0.log')
+log = Orocos::Log::Replay.open('~/rock/bundles/exoter/logs/20191204-1620/loccam.0.log', 
+                               '~/rock/bundles/exoter/logs/20191204-1620/imu.0.log',
+                               '~/rock/bundles/exoter/logs/20191204-1620/vicon.0.log')
 
 log.use_sample_time = true
 
-Orocos::Process.run 'viso2_with_imu::Task' => 'viso2_with_imu', 'spartan::Task' => 'spartan', 'viso2_evaluation::Task' => ['viso2_evaluation','viso2_evaluation_imu'] do
+Orocos::Process.run 'viso2_with_imu::Task' => 'viso2_with_imu', 'spartan::Task' => 'spartan', 'viso2_evaluation::Task' => 'viso2_evaluation_imu' do
 
     # CONFIGURE TASKS
-    viso2_evaluation = Orocos.name_service.get 'viso2_evaluation'
-    Orocos.conf.apply(viso2_evaluation, ['default'], :override => true)
-    viso2_evaluation.configure
-
     viso2_evaluation_imu = Orocos.name_service.get 'viso2_evaluation_imu'
     Orocos.conf.apply(viso2_evaluation_imu, ['default'], :override => true)
     viso2_evaluation_imu.configure
@@ -46,20 +42,17 @@ Orocos::Process.run 'viso2_with_imu::Task' => 'viso2_with_imu', 'spartan::Task' 
     log.camera_loccam.left_frame.connect_to             spartan.img_in_left 
     log.camera_loccam.right_frame.connect_to            spartan.img_in_right
 
-    log.vicon.pose_samples.connect_to                   viso2_evaluation.groundtruth_pose
-    spartan.vo_out.connect_to                           viso2_evaluation.odometry_pose
-    
     spartan.delta_vo_out.connect_to                     viso2_with_imu.delta_pose_samples_in
     log.imu_stim300.orientation_samples_out.connect_to  viso2_with_imu.pose_samples_imu
     
     viso2_with_imu.pose_samples_out.connect_to          viso2_evaluation_imu.odometry_pose
     log.vicon.pose_samples.connect_to                   viso2_evaluation_imu.groundtruth_pose
+    log.vicon.pose_samples.connect_to                   viso2_evaluation_imu.groundtruth_pose_not_aligned
     
 
     # START TASKS
     spartan.start
     viso2_with_imu.start
-    viso2_evaluation.start
     viso2_evaluation_imu.start
 
 
