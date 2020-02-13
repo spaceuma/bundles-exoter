d2f3df9885d4ce5f5e4f3d748d6e16434f4c23a7
Matteo De Benedetti <matteo.de.benedetti@esa.int>
2020-02-13 17:45:37 +0100

    Auto formatting

--- scripts/virtual_spartan.rb
+++ scripts/virtual_spartan.rb
@@ -12,11 +12,11 @@ Bundles.initialize
 tfse_file = '/home/marta/rock/bundles/rover/config/transforms_scripts_exoter.rb'
 Bundles.transformer.load_conf(tfse_file)
 
-# open log file 
-log = Orocos::Log::Replay.open('~/rock/bundles/exoter/logs/20200109-1452/loccam.0.log',
-                               '~/rock/bundles/exoter/logs/20200109-1452/imu.0.log',
-                               '~/rock/bundles/exoter/logs/20200109-1452/control.0.log',
-                               '~/rock/bundles/exoter/logs/20200109-1452/vicon.0.log')
+# open log file
+log = Orocos::Log::Replay.open('~/rock/bundles/exoter/logs/20200213-1741/loccam.0.log',
+                               '~/rock/bundles/exoter/logs/20200213-1741/imu.0.log',
+                               '~/rock/bundles/exoter/logs/20200213-1741/control.0.log',
+                               '~/rock/bundles/exoter/logs/20200213-1741/vicon.0.log')
 
 log.use_sample_time = true
 
@@ -26,7 +26,7 @@ Orocos::Process.run 'viso2_with_imu::Task' => 'viso2_with_imu', 'spartan::Task' 
     viso2_evaluation_imu = Orocos.name_service.get 'viso2_evaluation_imu'
     Orocos.conf.apply(viso2_evaluation_imu, ['default'], :override => true)
     viso2_evaluation_imu.configure
-    
+
     viso2_with_imu = TaskContext.get 'viso2_with_imu'
     Orocos.conf.apply(viso2_with_imu, ['default'], :override => true)
     viso2_with_imu.configure
@@ -37,19 +37,19 @@ Orocos::Process.run 'viso2_with_imu::Task' => 'viso2_with_imu', 'spartan::Task' 
     spartan.configure
 
     Orocos.log_all_ports
-    
-    
+
+
     # SET UP CONNECTIONS BETWEEN TASKS
-    log.camera_loccam.left_frame.connect_to             spartan.img_in_left 
+    log.camera_loccam.left_frame.connect_to             spartan.img_in_left
     log.camera_loccam.right_frame.connect_to            spartan.img_in_right
 
     spartan.delta_vo_out.connect_to                     viso2_with_imu.delta_pose_samples_in
     log.imu_stim300.orientation_samples_out.connect_to  viso2_with_imu.pose_samples_imu
-    
+
     viso2_with_imu.pose_samples_out.connect_to          viso2_evaluation_imu.odometry_pose
     log.vicon.pose_samples.connect_to                   viso2_evaluation_imu.groundtruth_pose
     log.vicon.pose_samples.connect_to                   viso2_evaluation_imu.groundtruth_pose_not_aligned
-    
+
 
     # START TASKS
     spartan.start
