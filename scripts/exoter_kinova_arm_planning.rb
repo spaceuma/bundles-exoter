61e6d68bcd05aa189ca28e30de1fb0d975344506
Laura Mantoani <laura.mantoani@uma.es>
2021-01-20 11:03:26 +0100

    Adapting the script and the configuration files to the new coupled control speed configuration

--- scripts/exoter_kinova_arm_planning.rb
+++ scripts/exoter_kinova_arm_planning.rb
@@ -56,45 +56,51 @@ Orocos.run 'navigation', 'control', 'simulation', 'vortex::Task' => 'vortex', 'p
 
     # Ports connection
     # Path planning outputs
-    path_planning.trajectory.connect_to                           kinova_planning.trajectory_port
+    path_planning.trajectory.connect_to                              kinova_planning.trajectory_port
 
     # Kinova planning outputs
-    kinova_planning.num_waypoints_port.connect_to                 coupled_control.size_path 
-    kinova_planning.modified_trajectory_port.connect_to           waypoint_navigation.trajectory
-    kinova_planning.joints_motionplanning_matrix_port.connect_to  coupled_control.arm_profile
-    kinova_planning.fetching_motionplanning_matrix_port.connect_to  coupled_control.final_movement_matrix_port
-    kinova_planning.final_movement_port.connect_to  coupled_control.kinova_final_movement_port
+    kinova_planning.num_waypoints_port.connect_to                    coupled_control.size_path 
+    kinova_planning.modified_trajectory_port.connect_to              waypoint_navigation.trajectory
+    kinova_planning.joints_motionplanning_matrix_port.connect_to     coupled_control.arm_profile
+    kinova_planning.fetching_motionplanning_matrix_port.connect_to   coupled_control.final_movement_matrix_port
+    kinova_planning.final_movement_port.connect_to                   coupled_control.kinova_final_movement_port
 
     # Vortex outputs
-    vortex.joints_readings.connect_to                             locomotion_control.joints_readings
-    vortex.pose.connect_to                                        waypoint_navigation.pose
- 	vortex.manipulator_readings.connect_to                        coupled_control.current_config_vector_double
-    vortex.manipulator_readings.connect_to                        kinova_planning.joints_position_port
-    vortex.pose.connect_to                                        path_planning.pose
-    vortex.pose.connect_to                                        kinova_planning.pose_port
-    vortex.goalWaypoint.connect_to                                path_planning.goalWaypoint
-    vortex.sample_position_port.connect_to                        kinova_planning.sample_position_port
-    vortex.sample_orientation_port.connect_to                     kinova_planning.sample_orientation_port
+    vortex.joints_readings.connect_to                                locomotion_control.joints_readings
+    vortex.pose.connect_to                                           waypoint_navigation.pose
+ 	vortex.manipulator_readings.connect_to                           coupled_control.current_config_vector_double
+    vortex.manipulator_readings.connect_to                           kinova_planning.joints_position_port
+    vortex.pose.connect_to                                           path_planning.pose
+    vortex.pose.connect_to                                           kinova_planning.pose_port
+    vortex.goalWaypoint.connect_to                                   path_planning.goalWaypoint
+    vortex.sample_position_port.connect_to                           kinova_planning.sample_position_port
+    vortex.sample_orientation_port.connect_to                        kinova_planning.sample_orientation_port
+    vortex.pose.connect_to                                           coupled_control.pose
 
     # Locomotion control outputs
-    locomotion_control.joints_commands.connect_to                 vortex.joints_commands
+    locomotion_control.joints_commands.connect_to                    vortex.joints_commands
 
     # Waypoint navigation outputs
-    waypoint_navigation.motion_command.connect_to                 coupled_control.motion_command
-    waypoint_navigation.current_segment.connect_to                coupled_control.current_segment
-    waypoint_navigation.trajectory_status.connect_to              kinova_planning.trajectory_status_port
+    waypoint_navigation.motion_command.connect_to                    coupled_control.motion_command
+    waypoint_navigation.current_segment.connect_to                   coupled_control.current_segment
+    waypoint_navigation.currentWaypoint.connect_to                   coupled_control.current_waypoint
+    waypoint_navigation.trajectory_status.connect_to                 kinova_planning.trajectory_status_port
 
     # Coupled control outputs
-    coupled_control.modified_motion_command.connect_to            locomotion_control.motion_command
-    coupled_control.manipulator_command_vector_double.connect_to  vortex.manipulator_commands
+    coupled_control.modified_motion_command.connect_to               locomotion_control.motion_command
+    coupled_control.manipulator_command_vector_double.connect_to     vortex.manipulator_commands
 
     # Starting programs
-    kinova_planning.start
     vortex.start
+
+    # Waiting one second to make sure all data from Vortex has been received
+    sleep(1)
+
+    kinova_planning.start
     coupled_control.start
+    path_planning.start
     waypoint_navigation.start
     locomotion_control.start
-    path_planning.start
 
 
     Readline::readline("Press ENTER to exit\n")
