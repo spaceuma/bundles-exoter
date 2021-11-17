1f1880bcc2b26aec6a8e440a5180bc007e520f38
Gonzalo J. Paz Delgado <gonzalopd96@uma.es>
2021-11-17 12:05:27 +0000

    Finish input interfacing with mpc_somp

--- scripts/tests/somp.rb
+++ scripts/tests/somp.rb
@@ -22,15 +22,16 @@ Orocos.run 'mpc_somp::Task' => 'mpc_somp' do
     # Start
     mpc_somp.start
 
+    # Dummy writing the input arm joints readings port
     arm_state_writer = mpc_somp.arm_joints.writer
 
     arm_joint_names = ["ARM_JOINT_1", "ARM_JOINT_2", "ARM_JOINT_3", "ARM_JOINT_4", "ARM_JOINT_5"]
 
     init_element = Types.base.JointState.new(
-                        position: Float::NAN,
-                        speed: Float::NAN,
-                        effort: Float::NAN,
-                        acceleration: Float::NAN)
+                        position: 0,
+                        speed: 0,
+                        effort: 0,
+                        acceleration: 0)
 
     arm_joint_elements = Array.new(5, init_element)
 
@@ -40,11 +41,11 @@ Orocos.run 'mpc_somp::Task' => 'mpc_somp' do
     arm_joint_elements[3] = Types.base.JointState.Position(1.5708)
     arm_joint_elements[4] = Types.base.JointState.Position(0.0)
 
-    arm_joint_elements[0].speed = 0.0
-    arm_joint_elements[1].speed = 0.0
-    arm_joint_elements[2].speed = 0.0
-    arm_joint_elements[3].speed = 0.0
-    arm_joint_elements[4].speed = 0.0
+    arm_joint_elements[0].speed = 0
+    arm_joint_elements[1].speed = 0
+    arm_joint_elements[2].speed = 0
+    arm_joint_elements[3].speed = 0
+    arm_joint_elements[4].speed = 0
 
     arm_reading = Types.base.commands.Joints.new(
                 time: Time.at(0),
@@ -53,8 +54,23 @@ Orocos.run 'mpc_somp::Task' => 'mpc_somp' do
 
     arm_state_writer.write(arm_reading)
 
+    # Dummy writing the input base joints readings port
+    base_state_writer = mpc_somp.base_joints.writer
+
+    motors_samples_names = ["WHEEL_DRIVE_FL", "WHEEL_DRIVE_FR", "WHEEL_DRIVE_CL", "WHEEL_DRIVE_CR", "WHEEL_DRIVE_BL", "WHEEL_DRIVE_BR", "WHEEL_STEER_FL", "WHEEL_STEER_FR", "WHEEL_STEER_BL", "WHEEL_STEER_BR", "WHEEL_WALK_FL", "WHEEL_WALK_FR", "WHEEL_WALK_CL", "WHEEL_WALK_CR", "WHEEL_WALK_BL", "WHEEL_WALK_BR"]
+
+    motors_samples_elements = Array.new(16, init_element)
+
+    motors_samples_reading = Types.base.commands.Joints.new(
+                time: Time.at(0),
+                names: motors_samples_names,
+                elements: motors_samples_elements)
+
+    base_state_writer.write(motors_samples_reading)
+
+    # Dummy writing the input end effector goal port
     goal_writer = mpc_somp.goal_ee_pose.writer
-    goal = Types::Base::Samples::RigidBodyState.new()
+    goal = Types.base.samples.RigidBodyState.new()
 
     goal.position[0] = 3.5
     goal.position[1] = 6.0
@@ -66,8 +82,9 @@ Orocos.run 'mpc_somp::Task' => 'mpc_somp' do
 
     goal_writer.write(goal)
 
+    # Dummy writing the input robot pose port
     pose_writer = mpc_somp.robot_pose.writer
-    pose = Types::Base::Samples::RigidBodyState.new()
+    pose = Types.base.samples.RigidBodyState.new()
 
     pose.position[0] = 1.5
     pose.position[1] = 6.0
